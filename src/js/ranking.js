b3a696197a6a5b8dcbb8692bc8df2d003dbd930d
Carlos Alberto Prost <carlosaprost@gmail.com>
2022-07-12 00:03:05 -0300

    cambios en la pantalla de Ranking

--- src/js/ranking.js
+++ src/js/ranking.js
@@ -1,5 +1,35 @@
-const btnBack = document.getElementById('back');
+const positions = document.getElementById('positions');
+let gammerPosition = [
+    {
+        "position": "1",
+        "name": "CAP",
+        "points": "8" 
+    },
+    {
+        "position": "1",
+        "name": "CAP",
+        "points": "8" 
+    },
+    {
+        "position": "1",
+        "name": "CAP",
+        "points": "8" 
+    },
+    {
+        "position": "1",
+        "name": "CAP",
+        "points": "8" 
+    }
+];
 
-btnBack.addEventListener('click', ()=>{
-    window.history.back();
-});
+
+
+for(let gammer of gammerPosition){
+    positions.innerHTML+= `
+    <tr>
+        <td>${gammer.position}</td>
+        <td>${gammer.name}</td>
+        <td>${gammer.points}</td>
+    </tr>
+    `;
+}
